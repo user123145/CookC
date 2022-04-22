6d0ef43a9739b649f8ecc89392b75c37daa74214
user123145 <shk9510@gmail.com>
2022-04-22 11:31:51 +0900

    /22.04.22 코드업1163

--- CodeUp/1163_220422_3/main.c
+++ CodeUp/1163_220422_3/main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+//코드업 1163
 int main()
 {
     int y,m,d;
-    scnanf(%d,&y);
-
-
+    scanf("%d %d %d",&y,&m,&d);
+    int x = y + m + d;
+    printf("그럭저럭");
+    x = (x/100)%10; //특정 자리수 구하기(백의자리),몫을 구하면 구하고자 하는 숫자가 1의자리에 온다, 그후 10으로 나머지 연산해서 숫자 알아낸다.
+    if ( x%2 ==0){
+        printf("대박");
+    }else{
+        printf("그럭저럭");
+    }
     return 0;
 }
--- CookC/Ex_A_12/main.c
+++ CookC/Ex_A_12/main.c
@@ -4,12 +4,14 @@ int main()
     int x,y;
     char c;
 
-    printf("첫 번째 수를 입력하세요 : ");
-    scanf("%d", &x);
-    printf("계산할 연산자를 입력하세요 : ");
-    scanf(" %c", &c);
-    printf("두 번째 수를 입력하세요 : ");
-    scanf("%d", &y);
+    // printf("첫 번째 수를 입력하세요 : ");
+    // scanf("%d", &x);
+    // printf("계산할 연산자를 입력하세요 : ");
+    // scanf(" %c", &c);
+    // printf("두 번째 수를 입력하세요 : ");
+    // scanf("%d", &y);
+    printf("첫번째 수, 연산자, 두번째 수를 입력하세요 : ");
+    scanf("%d %c %d",x,c,y);//한줄로도 받을수 있다.
 
     switch(c){
         case '+' :
