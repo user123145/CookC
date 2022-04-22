811ba92694e72344089560c764674b4cb1a821c5
user123145 <shk9510@gmail.com>
2022-04-22 09:54:22 +0900

    .

---
+++ Ex_CU_220422_1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//코드업 1156
+void main()
+{
+    int n;
+    scanf("%d", &n);
+
+    if (n % 2 == 0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+}
