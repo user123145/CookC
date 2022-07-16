db5aa124e5b26366f9a5290b7f194c88a5effcba
user123145 <shk9510@gmail.com>
2022-07-16 10:16:42 +0900

    실습 표준입출력 getchar

---
+++ CookC/Ex_11_06/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    char ch;
+
+    printf("문자열을 입력하세요 : ");
+    ch = getchar();
+    putchar(ch);
+
+    ch = getchar();
+    putchar(ch);
+
+    ch = getchar();
+    putchar(ch);
+}
